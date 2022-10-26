7fde3be52c4b200010c416d1228fe0fefe4321a1
Zeynep Sarıkaya <zynpsarkaya@gmail.com>
2022-10-26 20:32:10 +0300

    zeynep

--- src/test/java/Get02.java
+++ src/test/java/Get02.java
@@ -1,3 +1,44 @@
-public class Get02 {
+import io.restassured.response.Response;
+import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get02 {
+/* Given
+    https://restful-booker.herokuapp.com/booking/1
+    When
+    User send a GET Request to the url
+    Then
+    HTTP Status code should be 404
+    And
+    Status Line should be HTTP/1.1 404 Not Found
+    And
+    Response body contains "Not Found"
+    And
+    Response body does not contain "TechProEd"
+    And
+    Server is "Cowboy"
+            */
+
+    @Test
+    public void get01() {
+        //Set the url
+        String url="    https://restful-booker.herokuapp.com/booking/1";
+
+        //Set the Expected data
+
+        //Type code to send request
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        //Do Assertion
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+        //Body not found iceriyor mu
+        assertTrue(response.asString().contains("Not Found"));
+        //Body techproed icermedigi testi
+        assertFalse(response.asString().contains("TechProEd"));
+        //Server'in Cowboy olup olmadıgı testi
+        assertEquals("Cowboy",response.getHeader("Server"));
+    }
 }
---
+++ src/test/java/JsonplaceholderBaseUrl.java
@@ -0,0 +1,14 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+import org.junit.runner.Request;
+
+public class JsonplaceholderBaseUrl {
+    protected RequestSpecification spec;
+
+    @Before
+    public void setUp(){
+        spec=new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").build();
+
+    }
+}
