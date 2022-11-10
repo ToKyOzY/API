dfa596a15fd68eb8e3e4d1d111267ab6964d3382
Zeynep Sarıkaya <zynpsarkaya@gmail.com>
2022-11-10 14:16:59 +0300

    api

---
+++ src/test/java/get_request/Get05b.java
@@ -0,0 +1,34 @@
+package get_request;
+
+import base_url.ReqresBaseUrl;
+import org.junit.Test;
+
+public class Get05b extends ReqresBaseUrl {
+    /*
+        Given
+          https://reqres.in/api/unknown/3
+        When
+            User send a GET request to the URL
+        Then
+            HTTP Status Code should be 200
+        And
+            Response content type is “application/json; charset=utf-8”
+        And
+            Response body should be like;(Soft Assertion)
+        {
+        "data": {
+            "id": 3,
+            "name": "true red",
+            "year": 2002,
+            "color": "#BF1932",
+            "pantone_value": "19-1664"
+        },
+        "support": {
+            "url": "https://reqres.in/#support-heading",
+            "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+        }
+}
+      */
+    @Test
+    public void
+}
